2145cf2efbfe2f5151f7e720d6ca22620ba63d23
Melophylic <mnadzimtahara@gmail.com>
2026-04-30 23:48:21 +0700

    Remove duplicate accounts URL include to fix namespace warning

--- tiktaktuk_b8/urls.py
+++ tiktaktuk_b8/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("accounts.urls")),
     path("accounts/", include("accounts.urls")),
     path('venues/', include('venues.urls')),
     path('events/', include('events.urls')),
