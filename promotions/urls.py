c113de65970eca90edfd875c384c07fe7f71ca62
syafiq-f <syafiq.fqh@gmail.com>
2026-04-30 12:12:14 +0700

    benerin biar notfi n form jadi floating dan connectin ke dashbiard admin n organizer uhuyyy sudone done done sakses

--- promotions/urls.py
+++ promotions/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "promotions"
+
 urlpatterns = [
     path('', views.promotion_list, name='promotion_list'),
     path('create/', views.promotion_create, name='promotion_create'),
--- tiktaktuk_b8/urls.py
+++ tiktaktuk_b8/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("accounts.urls")),
     path("accounts/", include("accounts.urls")),
+    path("orders/", include("orders.urls")),
+    path("promotions/", include("promotions.urls")),
 ]
