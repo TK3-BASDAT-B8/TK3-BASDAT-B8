0fb9e6f8cab809e0f7329ed9501d0db9a8f68957
syafiq-f <syafiq.fqh@gmail.com>
2026-05-15 14:34:48 +0700

    coba lagi

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "BUILD START"
-python -m pip install -r requirements.txt
+python -m pip install -r requirements.txt --break-system-packages
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
